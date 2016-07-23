b9f3c123cf8eba25c769ec80918bcd1f61cc8f23
ZaleJ <947703213@qq.com>
2016-07-23 13:31:26 +0800

    Create InsertSort.java

---
+++ InsertSort.java
@@ -0,0 +1,19 @@
+public class InsertSort {
+    public static void main(String []args){
+        int [] array={20, 25, 15, 42, 36, 16, 12};
+        for(int i=1; i<array.length;++i){
+            int temp=array[i];
+            // 保存下标
+            int j=i;
+            while (j>0&&temp<array[j-1]){
+                // 上面数覆盖下面数
+                array[j]=array[j-1];
+                j--;
+            }
+            array[j]=temp;
+        }
+        for (int i = 0; i< array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
